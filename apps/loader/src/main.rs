513ed17c2cd9bb539a46dfd09880d45aaa8848a2
epoche <tniasu@qq.com>
2023-11-17 06:57:22 +0000

    step 02

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
+#![feature(asm_const)] 
+
 #[cfg(feature = "axstd")]
 use axstd::println;
 const PLASH_START: usize = 0x22000000;
@@ -10,7 +12,31 @@ fn main() {
     println!("Load payload ...");
     let code = unsafe { core::slice::from_raw_parts(apps_start, apps_size) };
     println!("content: {:#x}", bytes_to_usize(&code[..8]));
+
+    let load_code = unsafe { core::slice::from_raw_parts(apps_start, apps_size) };
+    println!(
+        "load code {:?}; address [{:?}]",
+        load_code,
+        load_code.as_ptr()
+    );
+    // app running aspace
+    // SBI(0x80000000) -> App <- Kernel(0x80200000)
+    // 0xffff_ffc0_0000_0000
+    const RUN_START: usize = 0xffff_ffc0_8010_0000;
+    let run_code = unsafe { core::slice::from_raw_parts_mut(RUN_START as *mut u8, apps_size) };
+    run_code.copy_from_slice(load_code);
+    println!("run code {:?}; address [{:?}]", run_code, run_code.as_ptr());
+
     println!("Load payload ok!");
+
+    println!("Execute app ...");
+    // execute app
+    unsafe { core::arch::asm!("
+        li      t2, {run_start}
+        jalr    t2
+        j       .",
+        run_start = const RUN_START,
+    )}
 }
 
 #[inline]
